c07913bd5e003fcb718c46ca2c36b255dfed2607
Daniel Alves <danialves2103@gmail.com>
2023-09-26 17:14:56 -0400

    sim

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import https from 'https'
+const fs = require("fs");
+const https = require ('https')
 const express = require('express');
 const app = express()
 const cors = require('cors') 
